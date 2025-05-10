d8ab9847fdc4f9b2086091ca283e25467cbbc139
Vorraphan <l.vorraphan@gmail.com>
2025-05-10 11:22:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,45 @@
 import streamlit as st
 
-st.title("HELLO, WELCOME TO THIS APP")
+st.titl("HELLO, WELCOME TO THIS APP")
+
 
 st.text("HI")
 
+ #include <stdio.h>                                                                                       
+
+int main()
+{
+    int c,l,e,a;scanf("%d %d %d %d",&c,&l,&e,&a);
+    int pay=0;
+    int promo;
+    
+    if(c>0){
+        pay=c*50;
+        promo=pay-(c*5);
+    if (pay<100){pay + 25;}
+    }
+    
+    if(l>0){
+        pay=l*45;
+        promo=pay-(l*5);
+    if (pay<100){pay + 25;}
+    }
+    
+    if(e>0){
+        pay=e*40;
+        promo=pay-(e*5);
+    if (pay<100){pay + 25;}
+    }
+    
+    if(a>0){
+        pay=a*35;
+        promo=pay-(a*5);
+    if (pay<100){pay + 25;}
+    }
+    
+    
+    printf("%d baht",pay);
+    
+
+    return 0;
+}
